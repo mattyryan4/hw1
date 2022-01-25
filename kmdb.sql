81a25c0c8623e85b0f6f07d609fafc2574826224
mattyryan4 <matt.ryan@kellogg.northwestern.edu>
2022-01-25 03:19:01 +0000

    Commit

--- kmdb.sql
+++ kmdb.sql
@@ -93,3 +93,20 @@
 
 -- The SQL statement for the cast output
 -- TODO!
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS top_cast;
+
+CREATE TABLE movies (
+    id INTEGER PRIMARY AUTOINCREMENT, 
+    title TEXT, 
+    year INTEGER, 
+    mpaa_rating TEXT, 
+    director TEXT
+);
+
+CREATE TABLE top_cast (
+    id INTEGER PRIMARY AUTOINCREMENT,
+    title TEXT, 
+    name TEXT, 
+    role TEXT, 
+);
